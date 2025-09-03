adfcff02e3003dbdc57e507f37ae365ceafd8703
Illya <151644034+Dragon0slayer@users.noreply.github.com>
2025-09-03 10:44:51 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,21 @@
+a = 10
+b = 1,1
+c = 'hello'
+p = [1,2,3]
+g = {1,2,3}
+h = (1,2,3,4)
+l = True
+m = { '1':"123", '2': '321'}
+s = 26
+d = 12.3
+print (a, type(a))
+print(d, type(d))
+print (g, type(g))
+print (p, type(p))
+print (h, type(h))
+print (c, type(c))
+print (l, type(l))
+print (b, type(b))
+print (m, type(m))
+print (s, type(s))
+

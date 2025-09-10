efa3cecf9bb98ceef94e7306aa42934c362a9ec3
Illya <151644034+Dragon0slayer@users.noreply.github.com>
2025-09-10 10:59:10 +0300

    Create 1.1.py

---
+++ 1/1/1.1.py
@@ -0,0 +1,21 @@
+age = 10
+money = 1,1
+text = 'hello'
+glossary = [1,2,3]
+glossary_2 = {1,2,3}
+glossary_3 = (1,2,3,4)
+value = True
+cataloge = { '1':"123", '2': '321'}
+age_2 = 26
+money_2 = 12.3
+print (age, type(age))
+print(money, type(money))
+print text(, type(text))
+print (glossary, type(glossary))
+print (glossary_2, type(glossary_2))
+print (glossary_3, type(glossary_3))
+print (value, type(value))
+print (cataloge, type(cataloge))
+print (age_2, type(age_2))
+print (money_2, type(money_2))
+
